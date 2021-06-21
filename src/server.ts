5618cf1b23ea5cd2effa80b4bc066fcf67d9e48b
RaulCesarCluster <raul@clusterdesign.com.br>
2021-06-21 19:01:24 -0300

    adicionado a primeira rota

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,9 @@ import express from 'express'
 
 const app = express();
 
+app.get('/first', (request, response) => {
+    return response.send('Ola NLW');
+});
+
+
 app.listen(3000, () => console.log('rodando server '))
