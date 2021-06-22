119a71dde3766afb0f15ecef6e8ba5f73db881e8
RaulCesarCluster <raul@clusterdesign.com.br>
2021-06-22 17:55:30 -0300

    criado a migration de usuario

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
-import express from 'express'
-import 'reflect-metadata';
-import './database';
+import express from "express";
+import "reflect-metadata";
+import "./database";
 
 const app = express();
 
-app.get('/first', (request, response) => {
-    return response.send('Ola NLW');
+app.get("/first", (request, response) => {
+  return response.send("Ola NLW");
 });
 
-
-app.listen(3000, () => console.log('rodando server '))
+app.listen(3000, () => console.log("rodando server "));
