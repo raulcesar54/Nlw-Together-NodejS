7a076393b8b3a96c2ae1ddf5b3ba58a626078867
RaulCesarCluster <raul@clusterdesign.com.br>
2021-06-23 20:44:31 -0300

    nome corrigido

--- src/controllers/createUser.controller.ts
+++ src/controllers/createUser.controller.ts
@@ -2,13 +2,12 @@ import { Request, response, Response } from 'express'
 import { CreateUserService } from '../services/CreateUser.service'
 
 class CreateUserController {
-  async handle(request: Request, reponse: Response) {
+  async handle(request: Request, response: Response) {
     const { name, email, admin } = request.body
-    console.log(request.body)
-
     const createUserService = new CreateUserService()
     const user = await createUserService.execute({ name, email, admin })
-    return reponse.json(user)
+
+    return response.json(user)
   }
 }
 
