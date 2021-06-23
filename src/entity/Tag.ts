3949d5420388455bcfecfa849b548c5e9f33957a
RaulCesarCluster <raul@clusterdesign.com.br>
2021-06-23 20:57:48 -0300

    criado a migration

---
+++ src/entity/Tag.ts
@@ -0,0 +1,15 @@
+// import { Entity } from 'typeorm'
+
+// @Entity('tags')
+// class Tag {
+//   constructor() {
+//     if (!this.id) {
+//       this.id = uuid()
+//     }
+//   }
+
+//   @PrimaryColumn()
+//   readonly id: string
+// }
+
+// export { Tag }
