d5e5809ab258ae1e80fbd6aeeb78026932f12066
RaulCesarCluster <raul@clusterdesign.com.br>
2021-06-26 11:25:17 -0300

    criado a rota de compliment

--- src/routes.ts
+++ src/routes.ts
@@ -3,14 +3,17 @@ import { CreateUserController } from './controllers/createUser.controller'
 import { CreateTagController } from './controllers/createTag.controller'
 import { AuthenticateUserController } from './controllers/authenticateUser.controller'
 import { ensureAdmin } from './middleware/ensureAdmin'
+import { CreateComplimentController } from './controllers/createCompliment.controller'
 const router = Router()
 
 const userController = new CreateUserController()
 const tagController = new CreateTagController()
 const authenticateUserController = new AuthenticateUserController()
+const complimentController = new CreateComplimentController()
 
 router.post('/user', userController.handle)
 router.post('/tag', ensureAdmin, tagController.handle)
 router.post('/auth', authenticateUserController.handle)
+router.post('/compliment', complimentController.handle)
 
 export { router }
